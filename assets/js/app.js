d67fb99df2f2fabb258433747491cccb18458343
devb0x <moniezgeoffrey59@gmail.com>
2021-07-03 16:33:09 +0200

    add json data

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+const search_input = document.getElementById('search')
+
+search_input.addEventListener('input', (e) => {
+  if (e.target.value.length >= 3) {
+    console.log(e.target.value)
+    
+  }
+})
