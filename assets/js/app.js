ddfef2b34f17ad378dcf58c6e356c1966deb116a
devb0x <moniezgeoffrey59@gmail.com>
2021-07-10 01:30:45 +0200

    add search function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,15 @@
-import { search } from "./search.js";
+import {resetDOM, search} from "./search.js";
 import { Recipe } from "./Recipe.js";
 
+const form = document.getElementById('form')
 const search_input = document.getElementById('search')
-
-search_input.addEventListener('input', (e) => {
-  if (e.target.value.length >= 3) {
-    // console.log(e.target.value)
-    search(e.target.value)
-  }
-})
+const recipes_section = document.querySelector('.recipes')
+const ingredients_btn = document.getElementById('ingredientsBtn')
 
 let recipes = []
+let ingredientsList = []
 
-function fetchAll() {
+export function fetchAll() {
   return fetch('https://raw.githubusercontent.com/devb0x/Moniezgeoffrey_7_03072021/master/assets/recipes.json')
     .then(response => {
       if (!response.ok) {
@@ -29,7 +26,7 @@ function fetchAll() {
 
 function renderAll() {
   recipes.forEach(el => {
-    const recipe = new Recipe(
+    new Recipe(
       el.id,
       el.name,
       el.servings,
@@ -42,6 +39,56 @@ function renderAll() {
   })
 }
 
+function renderIngredientList(ingredient) {
+  // console.log(ingredient)
+  const list = document.createElement('div')
+  list.innerHTML = `${ingredient}`
+  recipes_section.appendChild(list)
+}
+
+// const names = ['John', 'Paul', 'George', 'Ringo', 'John'];
+//
+// let unique = [...new Set(names)];
+
+function searchIngredients() {
+  let uniqueIngredientsList
+  console.log(recipes)
+  recipes.forEach(el => {
+    el.ingredients.forEach(ingredient => {
+      // console.log(ingredient.ingredient) // ok
+      ingredientsList.push(ingredient.ingredient)
+    })
+  })
+  uniqueIngredientsList = [...new Set(ingredientsList)];
+  renderIngredientList(uniqueIngredientsList)
+  // console.log(uniqueIngredientsList)
+  // console.log(ingredientsList)
+}
+
 fetchAll().then(() => {
   renderAll()
 })
+
+search_input.addEventListener('input', (e) => {
+  if (e.target.value.length >= 3) {
+    // console.log(e.target.value)
+    search(e.target.value)
+  }
+})
+
+ingredients_btn.addEventListener('click', (e) => {
+  console.log('searchIngredients()')
+  searchIngredients()
+})
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log('form submit')
+  if (search_input.value) {
+    // resetDOM()
+    console.log('value ok')
+    search(search_input.value)
+  }
+  // console.log(search_input.value)
+})
+
--- assets/js/search.js
+++ assets/js/search.js
@@ -1,3 +1,10 @@
+import {Recipe} from "./Recipe.js";
+
+const recipes_section = document.getElementsByClassName('recipes')
+const reset = document.getElementById('reset')
+
+let searchResult = []
+
 export function search(userInput) {
   return fetch('https://raw.githubusercontent.com/devb0x/Moniezgeoffrey_7_03072021/master/assets/recipes.json')
     .then(response => {
@@ -7,9 +14,64 @@ export function search(userInput) {
       return response.json()
     })
     .then(json => {
-      console.log(json)
-      console.log(userInput)
-      // json.filter(userInput)
-      console.log(json.recipes[0].id)
+      // Object.values(json.recipes).forEach(data => {
+        json.recipes.forEach(data => {
+        /**
+         * userInput est contenu dans le titre
+         */
+        if (data.name.toLowerCase().includes(userInput.toLowerCase())) {
+            searchResult.push(data)
+        }
+
+        /**
+         * userInput est contenu dans la description
+         */
+        if (data.description.toLowerCase().includes(userInput.toLowerCase())) {
+            searchResult.push(data)
+        }
+
+        /**
+         * boucle inside ingredients
+         * userInput est contenu dans un des ingredients
+         */
+        data.ingredients.forEach(el => {
+          if (el.ingredient.toLowerCase().includes(userInput.toLowerCase())) {
+              searchResult.push(data)
+          }
+        })
+      })
+    })
+    .then(() => {
+      searchResult = [...new Set(searchResult)]
+      console.log(searchResult)
+      resetDOM()
+      searchResult.forEach(el => {
+        new Recipe(
+          el.id,
+          el.name,
+          el.servings,
+          el.ingredients,
+          el.time,
+          el.description,
+          el.appliance,
+          el.ustensils
+        ).render()
+      })
+      searchResult = []
     })
 }
+
+/**
+ * Le système recherche des recettes correspondant à l’entrée utilisateur dans : le titre de
+ la recette, la liste des ingrédients de la recette, la description de la recette.
+ */
+
+export function resetDOM() {
+  // console.log('reset')
+  document.querySelectorAll(".recipe").forEach(e => e.parentNode.removeChild(e))
+  recipes_section.innerHTML = ''
+}
+
+reset.addEventListener('click', () => {
+  resetDOM()
+})
